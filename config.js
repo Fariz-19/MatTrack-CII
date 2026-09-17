a6b86cee02c1afda50db78c7255219768299f8b4
Fariz-19 <140682917+Fariz-19@users.noreply.github.com>
2026-09-17 22:31:51 +0700

    Set STORAGE_KEY_URL and STORAGE_KEY_KEY to real values
    Updated STORAGE_KEY_URL and STORAGE_KEY_KEY with actual values.

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const SUPABASE_CONFIG = {
 
   DEFAULT_ANON_KEY: 'sb_publishable_Fm9-W8tP7inInV5SYC2r9g_yFpX23Pc',
 
-  STORAGE_KEY_URL: 'stk_supabase_url',
-  STORAGE_KEY_KEY: 'stk_supabase_anon_key',
+  STORAGE_KEY_URL: 'https://uwzhnzzpoprejcrbglxu.supabase.co',
+  STORAGE_KEY_KEY: 'sb_publishable_Fm9-W8tP7inInV5SYC2r9g_yFpX23Pc',
   STORAGE_KEY_USER: 'stk_active_user',
   STORAGE_KEY_OFFLINE_TRX: 'stk_offline_transactions',
   STORAGE_KEY_OFFLINE_STOCK: 'stk_offline_stock'
